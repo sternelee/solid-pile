e4a727061a45718228383a7e75cd023645533c66
Sterne Lee <xingguang@clare.ai>
2024-03-29 12:05:54 +0800

    feat: update

--- src/markdown-it/index.ts
+++ src/markdown-it/index.ts
@@ -1,17 +1,26 @@
-import MarkdownIt from "markdown-it"
+import MarkdownIt from "markdown-it";
 // @ts-ignore
-import mdKatex from "markdown-it-katex"
-import mdHighlight from "markdown-it-highlightjs"
-import mdKbd from "markdown-it-kbd"
-import preWrapperPlugin from "./preWrapper"
+import mdKatex from "markdown-it-katex";
+import mdHighlight from "markdown-it-highlightjs";
+import mdKbd from "markdown-it-kbd";
+// import Shiki from "@shikijs/markdown-it";
+import preWrapperPlugin from "./preWrapper";
 
 export const md = MarkdownIt({
   linkify: true,
-  breaks: true
+  breaks: true,
 })
+  // .use(
+  //   await Shiki({
+  //     themes: {
+  //       light: "vitesse-light",
+  //       dark: "vitesse-dark",
+  //     },
+  //   }),
+  // )
   .use(mdKatex)
   .use(mdHighlight, {
-    inline: true
+    inline: true,
   })
   .use(mdKbd)
-  .use(preWrapperPlugin)
+  .use(preWrapperPlugin);
