c642861388b0daf8d9da947edfa68c27575b8e71
Sterne Lee <xingguang@clare.ai>
2024-04-27 14:18:17 +0800

    feat: review

--- app.config.ts
+++ app.config.ts
@@ -70,23 +70,18 @@ export default defineConfig({
           theme_color: "#f6f8fa",
           icons: [
             {
-              src: "icon.png",
+              src: "192.png",
               sizes: "192x192",
               type: "image/png",
             },
             {
-              src: "icon.png",
+              src: "256.png",
               sizes: "256x256",
               type: "image/png",
             },
             {
-              src: "icon.png",
+              src: "512.png",
               sizes: "512x512",
-              type: "image/png",
-            },
-            {
-              src: "icon.png",
-              sizes: "192x192",
               type: "image/png",
             },
           ],
