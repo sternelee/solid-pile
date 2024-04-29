989ccb688602e61ec0c1419662d575593cac1ae5
Sterne Lee <xingguang@clare.ai>
2024-04-29 14:54:05 +0800

    feat: save

--- src/routes/api/auth/[...solidauth].ts
+++ src/routes/api/auth/[...solidauth].ts
@@ -2,20 +2,23 @@ import { SolidAuth, type SolidAuthConfig } from '@solid-mediakit/auth'
 import GitHub from '@auth/core/providers/github'
 import Google from '@auth/core/providers/google'
 
+console.log('env:', process.env)
+console.log('map:', import.meta.env)
+
 const authOpts: SolidAuthConfig = {
   providers: [
     GitHub({
-      clientId: process.env.VITE_GITHUB_ID,
-      clientSecret: process.env.VITE_GITHUB_SECRET,
+      clientId: import.meta.env.VITE_GITHUB_ID,
+      clientSecret: import.meta.env.VITE_GITHUB_SECRET,
     }),
     Google({
-      clientId: process.env.VITE_GOOGLE_ID,
-      clientSecret: process.env.VITE_GOOGLE_SECRET,
+      clientId: import.meta.env.VITE_GOOGLE_ID,
+      clientSecret: import.meta.env.VITE_GOOGLE_SECRET,
     })
   ],
   debug: false,
-  secret: process.env.VITE_AUTH_SECRET,
-  trustHost: Boolean(process.env.VITE_AUTH_TRUST_HOST) || false
+  secret: import.meta.env.VITE_AUTH_SECRET,
+  trustHost: import.meta.env.VITE_AUTH_TRUST_HOST || false
 }
 
 export const { GET, POST } = SolidAuth(authOpts)
