25e390f2eaf0fbcbc7231d36463a41a2790e3b10
Sterne Lee <xingguang@clare.ai>
2024-04-29 15:02:59 +0800

    feat: 减少打包体积

--- src/layout/index.tsx
+++ src/layout/index.tsx
@@ -1,6 +1,6 @@
 import Header from "~/components/Header"
 import type { JSXElement } from "solid-js"
-import "katex/dist/katex.min.css";
+// import "katex/dist/katex.min.css";
 import "~/styles/main.css";
 import "~/styles/prism-material-dark.css"
 import "~/styles/prism-themes-modify.css"
